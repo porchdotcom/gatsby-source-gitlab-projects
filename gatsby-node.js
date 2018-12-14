d1c4fcab83f8f0bbdda3383c0fdd7eb1f03c4c47
Jason Nutter <jasondnutter@gmail.com>
2018-12-14 13:57:44 -0800

    Add support for fetching all projects for a Gitlab group

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,22 +4,36 @@ var path = require('path');
 var request = require('request-promise');
 
 const PROJECTS_PER_PAGE = 100; // gitlab max
-const getProjectRequest = (domain, privateToken, searchParams) => (page, resolveWithFullResponse) => (
-    request.get(
-        `${domain}/api/v4/projects`, {
-            headers: {
-                'Private-Token': privateToken
-            },
-            qs: {
-                archived: false,
-                simple: false,
-                page,
-                'per_page': PROJECTS_PER_PAGE,
-                ...searchParams
-            },
-            json: true,
-            resolveWithFullResponse
-        })
+
+const getProjectsRequest = (gitlabClient, searchParams) => (page, resolveWithFullResponse) => (
+    gitlabClient.get({
+        url: '/api/v4/projects',
+        qs: {
+            archived: false,
+            simple: false,
+            page,
+            'per_page': PROJECTS_PER_PAGE,
+            ...searchParams
+        },
+        resolveWithFullResponse
+    })
+);
+
+const getReadmeForBranch = (gitlabClient, projectId, readmeName, branch) => (
+    gitlabClient.get({
+        url: `/api/v4/projects/${projectId}/repository/files/${readmeName}/raw`,
+        qs: {
+            'ref': branch
+        }
+    })
+);
+
+const getProjectsForGroupRequest = (gitlabClient, groupId, searchParams) => (page, resolveWithFullResponse) => (
+    gitlabClient.get({
+        url: `/api/v4/groups/${groupId}/projects`,
+        qs: searchParams,
+        resolveWithFullResponse
+    })
 );
 
 exports.sourceNodes = async ({
@@ -33,18 +47,27 @@ exports.sourceNodes = async ({
             privateToken
         } = {},
         includeReadme = false,
-        searchParams = {}
+        searchParams = {},
+        groupId
     }) => {
     assert(domain, 'options.gitlab.domain object required');
     assert(privateToken, 'options.gitlab.privateToken object required');
 
-    const getProjects = getProjectRequest(domain, privateToken, searchParams);
-    const { headers, body } = await getProjects(1, true);
+    const gitlabClient = request.defaults({
+        baseUrl: domain,
+        headers: {
+            'Private-Token': privateToken
+        },
+        json: true
+    });
+
+    const getProjectsForPage = groupId ? getProjectsForGroupRequest(gitlabClient, groupId, searchParams) : getProjectsRequest(gitlabClient, searchParams);
+    const { headers, body } = await getProjectsForPage(1, true);
     const totalPages = parseInt(headers['x-total-pages'], 10);
 
     if (totalPages === 0) { return; }
 
-    const pagePromises = Array(...Array(totalPages - 1)).map((value, index) => getProjects(index + 2, false));
+    const pagePromises = Array(...Array(totalPages - 1)).map((value, index) => getProjectsForPage(index + 2, false));
     const projectResponses = await Promise.all(pagePromises);
     const projects = projectResponses.reduce((prev, cur) => prev.concat(cur), body);
 
@@ -63,34 +86,27 @@ exports.sourceNodes = async ({
         });
 
         if (includeReadme && project.readme_url) {
-            const readmeName = path.basename(project.readme_url || '');
-            readmePromises.push(request.get(
-                `${domain}/api/v4/projects/${project.id}/repository/files/${readmeName}/raw`, {
-                    headers: {
-                        'Private-Token': privateToken
-                    },
-                    qs: {
-                        'ref': project.default_branch
-                    }
-                })
-                .catch(() => console.error(`failed to fetch README for ${project.path_with_namespace}`))
-                .then(readme => {
-                    if (readme) {
-                        createNode({
-                            id: createNodeId(`gitlab-project-${project.id}-readme`),
-                            projectId: project.id,
-                            internal: {
-                                type: 'Readme',
-                                content: readme,
-                                mediaType: 'text/markdown',
-                                contentDigest: crypto
-                                    .createHash('md5')
-                                    .update(project.last_activity_at)
-                                    .digest('hex')
-                            }
-                        });
-                    }
-                })
+            const readmeName = path.basename(project.readme_url);
+            readmePromises.push(
+                getReadmeForBranch(gitlabClient, project.id, readmeName, project.default_branch)
+                    .catch(() => console.error(`failed to fetch README for ${project.path_with_namespace}`))
+                    .then(readme => {
+                        if (readme) {
+                            createNode({
+                                id: createNodeId(`gitlab-project-${project.id}-readme`),
+                                projectId: project.id,
+                                internal: {
+                                    type: 'Readme',
+                                    content: readme,
+                                    mediaType: 'text/markdown',
+                                    contentDigest: crypto
+                                        .createHash('md5')
+                                        .update(project.last_activity_at)
+                                        .digest('hex')
+                                }
+                            });
+                        }
+                    })
             );
         }
     });
